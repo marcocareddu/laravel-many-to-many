ccd80d63537fa5e423294ee31b8737bcc55e1a65
marcocareddu <saldumaccu@tuta.io>
2023-09-07 15:24:36 +0200

    Created All Technolgies CRUD & files

--- routes/web.php
+++ routes/web.php
@@ -69,3 +69,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::resource('admin/technologies', App\Http\Controllers\Admin\TechnologyController::class, ['as' => 'admin']);
