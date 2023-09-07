c24f76f7806d095fc4dfa1af4e8bf10e8552beab
marcocareddu <saldumaccu@tuta.io>
2023-09-07 16:00:11 +0200

    Filled Technology seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        $this->call([UserSeeder::class, Project::class, TypeSeeder::class]);
+        $this->call([UserSeeder::class, TechnologySeeder::class, Project::class, TypeSeeder::class]);
     }
 }
