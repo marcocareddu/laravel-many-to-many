e546f5a3207c92a3bb5202a8d726b490f3fd8916
marcocareddu <saldumaccu@tuta.io>
2023-09-07 15:12:15 +0200

    Added foreign keys to project_technology table

--- database/migrations/2023_09_07_125505_create_project_technology_table.php
+++ database/migrations/2023_09_07_125505_create_project_technology_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->foreignId('technology_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
